618ded61ff7ba684854dba0366617f0d07e52ea1
Vishvajeet Singh <109080799+Vishvajeet123@users.noreply.github.com>
2025-05-13 19:30:10 +0530

    Time: 8 ms (52.84%), Space: 63.3 MB (95.61%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function(x) {
+    
+    if(x<0){
+        return false;
+    }
+
+
+
+  let y = String(x).split("")
+        for(let i=0;i<y.length;i++){
+
+            if(y[i]!=y[y.length-1-i]){
+                return false ;
+            }
+          
+        }
+
+
+    return true; 
+
+};
